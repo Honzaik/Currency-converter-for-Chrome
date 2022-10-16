ded2c9cf81a3a521d7dd579f9ba7319cc22cf113
Honzaik <honzaik@seznam.cz>
2022-10-16 14:42:34 +0200

    mv3 supports, rounding setting, jquery update

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Currency-converter-for-Chrome
-Currency converter for Chrome by Honzaik
+# Currency converter by Honzaik
+Currency converter by Honzaik
+
+Due to Chrome manifest v3 bullshit there are 2 different manifests v2 (for firefox) and v3 (for chrome). Rename accordingly if you use this repo. 
 
 https://chrome.google.com/webstore/detail/currency-converter-by-hon/lipikfhaddhanogjjdbdmjfohoaepkmi
+https://addons.mozilla.org/en-US/firefox/addon/currency-converter-by-honzaik/
 
--- popup.js
+++ popup.js
@@ -3,34 +3,41 @@ currencyNamesArray = [];
 currencyRates = [];
 lastInput = null;
 infoShown = false;
+settingsShown = false;
 browserObject = null;
+defaultRoundingDecimals = 10;
+roundingDecimals = defaultRoundingDecimals;
 
 $(document).ready(function() {
-    if(typeof chrome !== 'undefined'){
-      browserObject = chrome;
-    }else{
-      browserObject = browser;
-    }
-    console.log(browserObject);
-    fetchCurrencies();
+  if(typeof chrome !== 'undefined'){
+    browserObject = chrome;
+  }else{
+    browserObject = browser;
+  }
+  fetchCurrencies();
 });
 
 
 function readSettings(){
-	savedCurrencies = browserObject.storage.local.get("currencies", function(result){
-    	if(result["currencies"] == "" || result["currencies"] == null){
-	    	setCurrencies(["usd", "eur"]);
-	    }else{
-	    	setCurrencies(result["currencies"]);
-	    }
-    });
-    $(".loading").slideUp(500);
+  browserObject.storage.local.get(["currencies", "roundingDecimals"], function(result){
+    if(result["roundingDecimals"] != "" && result["roundingDecimals"] != null){
+      roundingDecimals = parseInt(result["roundingDecimals"]);
+    }
+    $("#setting-decimals").val(roundingDecimals);
+    $("#setting-decimals").next().toggleClass("active");
+    if(result["currencies"] == "" || result["currencies"] == null){
+      setCurrencies(["usd", "eur"]);
+    }else{
+      setCurrencies(result["currencies"]);
+    }
+  });
+  $(".loading").slideUp(500);
 }
 
 function parseCurrencyName(name){
-  console.log("parsing " + name);
+  //console.log("parsing " + name);
   var parts = name.split("-");
-  console.log(parts);
+  //console.log(parts);
   var parsedName = parts[1].charAt(0).toUpperCase() + parts[1].slice(1);
   if(parts.length > 2){
     parsedName += " " + parts[2].charAt(0).toUpperCase() + parts[2].slice(1);
@@ -63,8 +70,8 @@ function cacheJSON(json){
 }
 
 function fetchCurrencies(){
-	currencyArray = [];
-	currencyNamesArray = [];
+  currencyArray = [];
+  currencyNamesArray = [];
   currencyJSON = null;
   browserObject.storage.local.get("currencyJSON", function(result){
     resultJSON = result["currencyJSON"];
@@ -85,7 +92,7 @@ function fetchCurrencies(){
         },
       });
     }else{
-      console.log("cached");
+      //console.log("cached");
       parseCurrencyJSON(resultJSON.currencyJSON);
       readSettings();
     }
@@ -93,11 +100,11 @@ function fetchCurrencies(){
 }
 
 function populateOptions(symbolArray, nameArray){
-	currencySelect = $("select.currency-select");
-	$(currencySelect).each(function(){
-		console.log($(this).children().length);
-		if($(this).children().length == 1){
-			for(i = 0; i < symbolArray.length; i++){
+  currencySelect = $("select.currency-select");
+  $(currencySelect).each(function(){
+    //console.log($(this).children().length);
+    if($(this).children().length == 1){
+      for(i = 0; i < symbolArray.length; i++){
         var option = document.createElement('option');
         option.textContent = nameArray[i];
         option.value = symbolArray[i];
@@ -141,7 +148,7 @@ $("#button-remove").click(function(){
 });
 
 $("#currencies").on("keyup", "input.amount", function(){
-	recalculate(this);
+  recalculate(this);
 });
 
 function recalculate(input){
@@ -155,7 +162,7 @@ function recalculate(input){
     }
   });
 
-  console.log(currenciesToConvert);
+  //console.log(currenciesToConvert);
 
   if(!isNaN(amount) && currency != '' && amountInputs.length > 1){ // input is a number
     convertedAmounts = [];
@@ -169,7 +176,8 @@ function recalculate(input){
 
 function convertCurrency(from, to, amount){
   var fromInUSD = amount*currencyRates[from].inverseRate;
-  $("input#" + to).val(fromInUSD*currencyRates[to].rate);
+  var value = Number(Math.round(fromInUSD*currencyRates[to].rate + 'e' + roundingDecimals) + 'e-' + roundingDecimals);
+  $("input#" + to).val(value);
 }
 
 function setCurrencies(array){
@@ -186,7 +194,7 @@ function setCurrencies(array){
   for(i = 0; i < array.length; i++){
     $(amountInputs[i]).attr("id", array[i]);
     $(selects[i]).find("option[value='" + array[i] + "']").prop('selected', true);
-    console.log($(selects[i]).find("option[value='" + array[i] + "']").html());
+    //console.log($(selects[i]).find("option[value='" + array[i] + "']").html());
   }
   $("select").formSelect();
 }
@@ -194,14 +202,26 @@ function setCurrencies(array){
 function saveSettings(){
   currencies = [];
   amountInputs = $("input.amount");
+  var toSave = {}
   $(amountInputs).each(function(){
     if ($(this).attr("id") != ''){
       currencies.push($(this).attr("id"));
     }
   });
   if(currencies != null && currencies != ''){
-    browserObject.storage.local.set({"currencies" : currencies});
+    toSave.currencies = currencies;
   }
+  var value = $("#setting-decimals").val();
+  if (value == '' || value == null) {
+    roundingDecimals = defaultRoundingDecimals
+  } else {
+    roundingDecimals = parseInt(value);
+  }
+  if (roundingDecimals < 0 || roundingDecimals > 20) {
+    roundingDecimals = defaultRoundingDecimals;
+  }
+  toSave.roundingDecimals = roundingDecimals;
+  browserObject.storage.local.set(toSave);
 }
 
 $(".heading").click(function(){
@@ -212,4 +232,18 @@ $(".heading").click(function(){
     $(".info").slideUp(500);
     infoShown = false;
   }
+});
+
+$("#button-settings").click(function(){
+  if(!settingsShown){
+    $("#settings").slideDown(500);
+    settingsShown = true;
+  }else{
+    $("#settings").slideUp(500);
+    settingsShown = false;
+  }
+});
+
+$("#button-save-setting").click(function(){
+  saveSettings();
 });
